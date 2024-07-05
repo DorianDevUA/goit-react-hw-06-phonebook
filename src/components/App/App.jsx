d006d6b42abf6a0f7b6ed9c524e88c8ba7b68555
Dorian Devua <Dorian.DevUa@gmail.com>
2024-07-05 15:18:14 +0400

    add Modal component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,27 @@
+import { useDispatch, useSelector } from 'react-redux';
+import Modal from '../Modal';
+import { toggleModal } from '../../redux/modal/actions';
+
 const App = () => {
+  const { showModal } = useSelector(state => state.modal);
+  const dispatch = useDispatch();
+
   return (
     <>
+      {showModal && (
+        <Modal>
+          <p>This text from Modal</p>
+          <button
+            type="button"
+            onClick={() => dispatch(toggleModal(showModal))}
+          >
+            Toggle Modal
+          </button>
+        </Modal>
+      )}
+      <button type="button" onClick={() => dispatch(toggleModal(showModal))}>
+        Toggle Modal
+      </button>
       <h1>Vite + React + Redux</h1>
       <p>Click on the Vite and React logos to learn more</p>
     </>
---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,47 @@
+import { PropTypes } from 'prop-types';
+import { useEffect } from 'react';
+import { createPortal } from 'react-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { getShowModal } from '../../redux/contacts/selectors';
+import { toggleModal } from '../../redux/modal/actions';
+import { Backdrop, ModalContent } from './Modal.styled';
+
+const rootModal = window.document.getElementById('root-modal');
+
+const Modal = ({ children }) => {
+  const showModal = useSelector(getShowModal);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const handleKeyDown = evt => {
+      if (evt.code === 'Escape') {
+        dispatch(toggleModal(showModal));
+      }
+    };
+
+    window.document.addEventListener('keydown', handleKeyDown);
+
+    return () => {
+      window.document.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [dispatch, showModal]);
+
+  const handleBackdropClick = evt => {
+    if (evt.target === evt.currentTarget) {
+      dispatch(toggleModal(showModal));
+    }
+  };
+
+  return createPortal(
+    <Backdrop onClick={handleBackdropClick}>
+      <ModalContent>{children}</ModalContent>
+    </Backdrop>,
+    rootModal,
+  );
+};
+
+Modal.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default Modal;
