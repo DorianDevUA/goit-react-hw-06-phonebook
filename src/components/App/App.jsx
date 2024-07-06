b5de721f402c210b70457302c06fb6c9071a7881
Dorian Devua <Dorian.DevUa@gmail.com>
2024-07-06 17:34:55 +0400

    add render ContactList

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,8 @@
 import { useDispatch, useSelector } from 'react-redux';
-import Modal from '../Modal';
 import { toggleModal } from '../../redux/modal/actions';
+import Modal from '../Modal';
 import ContactForm from '../ContactForm';
+import ContactList from '../ContactList';
 
 const App = () => {
   const { showModal } = useSelector(state => state.modal);
@@ -19,6 +20,7 @@ const App = () => {
       </button>
       <h1>Vite + React + Redux</h1>
       <p>Click on the Vite and React logos to learn more</p>
+      <ContactList />
     </>
   );
 };
