b160b7c9f2ef309135bdb1de3f92a24801e6fa83
Dorian Devua <Dorian.DevUa@gmail.com>
2024-07-06 18:50:03 +0400

    add getFilteredContacts func

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,14 +1,26 @@
 import { useSelector } from 'react-redux';
 import { getContacts } from '../../redux/contacts/selectors';
+import { getFilterValue } from '../../redux/filter/selectors';
 import ContactItem from '../ContactItem';
 import { Contact, StyledList } from './ContactList.styled';
 
 const ContactList = () => {
   const contacts = useSelector(getContacts);
+  const filterValue = useSelector(getFilterValue);
+
+  const getFilteredContacts = () => {
+    const normalizedFilter = filterValue.toLowerCase();
+
+    return contacts.filter(({ name }) =>
+      name.toLowerCase().includes(normalizedFilter),
+    );
+  };
+
+  const filtredContacts = getFilteredContacts();
 
   return (
     <StyledList>
-      {contacts.map(contact => (
+      {filtredContacts.map(contact => (
         <Contact key={contact.id}>
           <ContactItem contact={contact} />
         </Contact>
