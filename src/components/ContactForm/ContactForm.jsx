587ef24b9abea9dab34f13f8420cde102af16f4e
Dorian Devua <Dorian.DevUa@gmail.com>
2024-07-07 13:08:59 +0400

    add FormError component

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -3,7 +3,8 @@ import { useDispatch, useSelector } from 'react-redux';
 import { getShowModal } from '../../redux/modal/selectors';
 import { toggleModal } from '../../redux/modal/actions';
 import { addContact } from '../../redux/contacts/actions';
-import { Field, Formik, ErrorMessage } from 'formik';
+import { Field, Formik } from 'formik';
+import FormError from '../FormError';
 import * as Yup from 'yup';
 import {
   AddContactBtn,
@@ -18,7 +19,10 @@ const phoneRegExp =
   /^[+]?3?[\s]?8?[\s]?\(?0\d{2}?\)?[\s]?\d{3}[\s|-]?\d{2}[\s|-]?\d{2}$/;
 
 const schema = Yup.object().shape({
-  name: Yup.string().min(2).max(16).required(),
+  name: Yup.string()
+    .min(2, 'Too short!')
+    .max(32, 'Too long!')
+    .required('Required'),
   number: Yup.string()
     .matches(phoneRegExp, 'Phone number is not valid')
     .required(),
@@ -54,13 +58,13 @@ const ContactForm = () => {
         <StyledLabel htmlFor={nameImputId}>
           <LabelTitle>Name</LabelTitle>
           <Field type="text" name="name" id={nameImputId} />
-          <ErrorMessage component="div" name="name" />
+          <FormError name="name" />
         </StyledLabel>
 
         <StyledLabel htmlFor={numberImputId}>
           <LabelTitle>Number</LabelTitle>
           <Field type="tel" name="number" id={numberImputId} />
-          <ErrorMessage component="div" name="number" />
+          <FormError name="number" />
         </StyledLabel>
 
         <AddContactBtn type="submit">Add contact</AddContactBtn>
